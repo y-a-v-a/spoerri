b3ff537f75d5feb5c583c518ddd7a9d994d7ca72
Vincent Bruijn <vebruijn@gmail.com>
2018-01-08 21:49:05 +0100

    expressified app

---
+++ app.js
@@ -0,0 +1,64 @@
+const fetchRunner = require('./lib/fetch-runner');
+const spoerriRunner = require('./lib/spoerri-runner');
+
+const glob = require('glob');
+const debug = require('debug')('app');
+
+const express = require('express');
+const app = express();
+
+const oSearchKeywords = require('./keywords');
+fetchRunner.run(oSearchKeywords);
+
+spoerriRunner.run();
+
+app.get('/spoerri.jpg', (req, res, next) => {
+  const files = glob('./tmp/**.jpg', {}, (error, files) => {
+    if (error) {
+      throw error;
+    }
+
+    const randomIndex = Math.round(Math.random() * files.length);
+    const fileName = files[randomIndex];
+    debug(fileName);
+
+    res.sendFile(fileName, {
+      root: __dirname,
+      dotfiles: 'deny',
+      headers: {
+        'Content-Type': 'image/jpeg'
+      }
+    }, (error) => {
+      if (error) {
+        next(error);
+      }
+    });
+  });
+});
+
+app.get('/favicon.png', (req, res, next) => {
+  res.sendFile('./favicon.png', {
+    root: __dirname,
+    dotfiles: 'deny',
+    headers: {
+      'Content-Type': 'image/png'
+    }
+  }, (error) => {
+    if (error) next(error);
+  });
+});
+
+app.get('/', (req, res, next) => {
+  res.sendFile('./index.html', {
+    root: __dirname,
+    dotfiles: 'deny',
+    headers: {
+      'Content-Type': 'text/html'
+    }
+  }, (error) => {
+    if (error) next(error);
+  })
+});
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
