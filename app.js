887c2e5987c5aad1ea7bd26262db66173942605d
Vincent Bruijn <vebruijn@gmail.com>
2021-09-16 22:05:58 +0200

    awsify code

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const fetchRunner = require('./lib/fetch-runner');
-const spoerriRunner = require('./lib/spoerri-runner');
+require('dotenv').config();
 
-const glob = require('glob');
-const debug = require('debug')('app');
+const fetchRunner = require('./lib/fetch-runner');
+// const spoerriRunner = require('./lib/spoerri-runner');
+const getRandomResult = require('./lib/result-fetch');
+
+const debug = require('debug')('spoerri:app');
 
 const express = require('express');
 const app = express();
@@ -10,83 +12,76 @@ const app = express();
 const oSearchKeywords = require('./keywords');
 fetchRunner.run(oSearchKeywords);
 
-spoerriRunner.run();
+// spoerriRunner.run();
 
-app.get('/spoerri.jpg', (req, res, next) => {
-  const files = glob('./tmp/**.jpg', {}, (error, files) => {
-    if (error) {
-      throw error;
-    }
-
-    const randomIndex = Math.floor(Math.random() * files.length);
-    const fileName = files[randomIndex];
-    debug(fileName);
-
-    if (!fileName) {
-      return next();
-    }
-
-    res.sendFile(fileName, {
-      root: __dirname,
-      dotfiles: 'deny',
-      headers: {
-        'Content-Type': 'image/jpeg'
-      }
-    }, (error) => {
-      if (error) {
-        next(error);
-      }
-    });
-  });
+app.get('/spoerri.jpg', async (req, res, next) => {
+  return (await getRandomResult()).pipe(res);
 });
 
 app.get('/favicon.png', (req, res, next) => {
-  res.sendFile('./public/favicon.png', {
-    root: __dirname,
-    dotfiles: 'deny',
-    headers: {
-      'Content-Type': 'image/png'
+  res.sendFile(
+    './public/favicon.png',
+    {
+      root: __dirname,
+      dotfiles: 'deny',
+      headers: {
+        'Content-Type': 'image/png',
+      },
+    },
+    (error) => {
+      if (error) next(error);
     }
-  }, (error) => {
-    if (error) next(error);
-  });
+  );
 });
 
 app.get('/robots.txt', (req, res, next) => {
-  res.sendFile('./public/robots.txt', {
-    root: __dirname,
-    dotfiles: 'deny',
-    headers: {
-      'Content-Type': 'text/plain'
+  res.sendFile(
+    './public/robots.txt',
+    {
+      root: __dirname,
+      dotfiles: 'deny',
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    },
+    (error) => {
+      if (error) next(error);
     }
-  }, (error) => {
-    if (error) next(error);
-  });
+  );
 });
 
 app.get('/style.css', (req, res, next) => {
-  res.sendFile('./public/style.css', {
-    root: __dirname,
-    dotfiles: 'deny',
-    headers: {
-      'Content-Type': 'text/css'
+  res.sendFile(
+    './public/style.css',
+    {
+      root: __dirname,
+      dotfiles: 'deny',
+      headers: {
+        'Content-Type': 'text/css',
+      },
+    },
+    (error) => {
+      if (error) next(error);
     }
-  }, (error) => {
-    if (error) next(error);
-  });
+  );
 });
 
 app.get('/', (req, res, next) => {
-  res.sendFile('./public/index.html', {
-    root: __dirname,
-    dotfiles: 'deny',
-    headers: {
-      'Content-Type': 'text/html'
+  res.sendFile(
+    './public/index.html',
+    {
+      root: __dirname,
+      dotfiles: 'deny',
+      headers: {
+        'Content-Type': 'text/html',
+      },
+    },
+    (error) => {
+      if (error) next(error);
     }
-  }, (error) => {
-    if (error) next(error);
-  })
+  );
 });
 
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () =>
+  console.log('Example app listening on port http://localhost:3000 !')
+);
--- readme.md
+++ readme.md
@@ -6,7 +6,40 @@ Tableau Piège - Snare Picture
 
 A "Snare Picture" online results in a "Web Snare"
 
-
 ## License
 
 GNU GPL v3, copyright 2017-2018 ax710.org && y-a-v-a.org
+
+```javascript
+app.get('/spoerri2.jpg', (req, res, next) => {
+  const files = glob('./tmp/**.jpg', {}, (error, files) => {
+    if (error) {
+      throw error;
+    }
+
+    const randomIndex = Math.floor(Math.random() * files.length);
+    const fileName = files[randomIndex];
+    debug(fileName);
+
+    if (!fileName) {
+      return next();
+    }
+
+    res.sendFile(
+      fileName,
+      {
+        root: __dirname,
+        dotfiles: 'deny',
+        headers: {
+          'Content-Type': 'image/jpeg',
+        },
+      },
+      (error) => {
+        if (error) {
+          next(error);
+        }
+      }
+    );
+  });
+});
+```
